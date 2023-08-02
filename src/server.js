7cbac69b016f5fbb35ce2ccccb768b89105c18ae
galego-vinicius <vfelix1998@gmail.com>
2023-08-02 19:36:28 -0300

    Remove-Update-Capture-Filter-applicationRoute

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 // CommonJS => require
 // ESModules => import/export
@@ -60,6 +61,12 @@ const server = http.createServer(async (req, res) => {
 
     if (route) {
         const routeParams = req.url.match(route.path)
+
+        // console.log(extractQueryParams(routeParams.groups.query))
+
+        const {query, ...params} = routeParams.groups
+        req.params = params
+        req.query = query ? extractQueryParams(query): {}
 
         return route.handler(req, res)
     }
